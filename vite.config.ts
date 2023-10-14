16c792d9235111c01a532327b954dc96da0d1564
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-10-14 15:29:40 +0800

    chore: update the alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteEslint from 'vite-plugin-eslint';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteEslint()],
   resolve: {
     alias: {
-      '@': '/src'
+      '@': path.join(__dirname, 'src')
     }
   }
 });
