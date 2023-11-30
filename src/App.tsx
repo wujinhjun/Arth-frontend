ab0266c2de85687544d5ef8ff20965ba06a3c31f
wujinhjun <wujinhjun@outlook.com>
2023-12-01 00:08:26 +0800

    refactor: 重构

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,124 @@
+import React, { useState } from 'react';
+
+import { NavLink, useRoutes } from 'react-router-dom';
+
 import {
-  createBrowserRouter,
-  RouterProvider,
-  useLoaderData
-} from 'react-router-dom';
+  Bars3CenterLeftIcon,
+  HomeIcon,
+  MapIcon,
+  ChartBarIcon,
+  CreditCardIcon,
+  PaintBrushIcon
+} from '@heroicons/react/24/outline';
+
 import HomePage from '@/pages/homePage';
 import TxtImgPage from './pages/txtImgPage';
+import EEGStudioPage from './pages/EEGstudioPage';
+import PaintingPage from './pages/paintingsPage';
+import styles from './styles/app.module.css';
 
 function App() {
-  const router = createBrowserRouter([
+  const routes = useRoutes([
     {
       path: '/',
-      Component() {
-        return (
-          <>
-            <HomePage />
-          </>
-        );
-      }
+      element: <HomePage />
+    },
+    {
+      path: '/home',
+      element: <HomePage />
     },
     {
       path: '/img',
-      Component() {
-        return (
-          <>
-            <TxtImgPage />
-          </>
-        );
-      }
+      element: <TxtImgPage />
+    },
+    {
+      path: 'eeg',
+      element: <EEGStudioPage />
+    },
+    {
+      path: 'painting',
+      element: <PaintingPage />
     }
   ]);
-  return <RouterProvider router={router} />;
+
+  const [navbarList] = useState([
+    { key: 'home', icon: HomeIcon },
+    { key: 'painting', icon: PaintBrushIcon },
+    { key: 'map', icon: MapIcon },
+    { key: 'chart', icon: ChartBarIcon },
+    { key: 'credit', icon: CreditCardIcon }
+  ]);
+  const [activeNavItem, setActiveNavItem] = useState<string>('home');
+
+  const handleChangeActiveNavItem = (
+    e: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    let element = e.target as HTMLElement;
+    let parentElement = element?.parentElement;
+
+    if (!parentElement) {
+      return;
+    }
+
+    while (parentElement && parentElement.tagName !== 'NAV') {
+      element = parentElement;
+      parentElement = element?.parentElement;
+    }
+
+    setActiveNavItem(element.id);
+    // window.location.href = `/${element.id}`;
+  };
+
+  return (
+    <section className="page-container">
+      <div className={`${styles['left-panel']}`}>
+        <div className={`${styles['left-top-button-wrapper']}`}>
+          <input
+            type="checkbox"
+            name=""
+            id="left-button-panel"
+            className={`${styles.input}`}
+          />
+          <label htmlFor="left-button-panel" className="w-fit h-fit">
+            <Bars3CenterLeftIcon
+              className={'mx-auto rotate-180  cursor-pointer'}
+            />
+          </label>
+        </div>
+
+        <nav
+          className={`${styles['left-navbar-wrapper']}`}
+          onClick={handleChangeActiveNavItem}
+        >
+          {navbarList.map((item) => {
+            return (
+              <NavLink
+                // unstable_viewTransition
+                to={`/${item.key}`}
+                className={`${styles['left-navbar-button']} ${
+                  item.key === activeNavItem ? 'active' : ''
+                }`}
+                key={item.key}
+                id={item.key}
+              >
+                <item.icon
+                  className={`${styles['left-navbar-button-icon']}`}
+                  id={item.key}
+                />
+              </NavLink>
+            );
+          })}
+        </nav>
+
+        <div className={`${styles['left-bottom-buttons-wrapper']}`}>
+          <button className={`${styles['left-bottom-button']}`}>
+            <img src="" alt="" />
+          </button>
+        </div>
+      </div>
+      <div className="right-panel">{routes}</div>
+    </section>
+  );
 }
 
 export default App;
--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,24 +1,33 @@
 import styles from '@/styles/homepage.module.css';
-import { Bars3CenterLeftIcon } from '@heroicons/react/24/outline';
+import picture from '@/assets/home/RenaissanceFirst.png';
+
+interface paintingItemType {
+  key: string;
+  name: string;
+  img: string;
+  introduction: string;
+  author: string;
+}
 
 export default function Homepage() {
   return (
-    <section className={`${styles.wrapper}`}>
-      <div className={`${styles['left-panel']}`}>
-        <div className={`${styles['left-top-button-wrapper']}`}>
-          <Bars3CenterLeftIcon
-            className={'mx-auto rotate-180  cursor-pointer'}
-          />
-        </div>
-        <div className={`${styles['left-bottom-buttons-wrapper']}`}>
-          <button className={`${styles['left-bottom-button']}`}>a</button>
-          <button className={`${styles['left-bottom-button']}`}>b</button>
-        </div>
-      </div>
+    <>
+      {/* right panel */}
       <div className={`${styles['right-panel']}`}>
-        <div className={`${styles['right-panel-content-area']}`}></div>
+        <div className={`${styles['right-panel-content-area']}`}>
+          <div
+            className={`${styles['right-panel-content-list-wrapper']}`}
+          ></div>
+
+          <div className={`${styles['right-panel-content-img']}`}>
+            <img src={picture} alt="" />
+          </div>
+          <div
+            className={`${styles['right-panel-content-intro-wrapper']}`}
+          ></div>
+        </div>
         <div className={`${styles['right-panel-timescale']}`}></div>
       </div>
-    </section>
+    </>
   );
 }
