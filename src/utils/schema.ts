35998b27972092431e1ba033d3773443e5fe4c96
wujinhjun <wujinhjun@outlook.com>
2023-12-12 00:38:34 +0800

    feat: the schema for GenerateImage

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -13,3 +13,19 @@ export const createImageSchema = z.object({
   negativePrompt: z.optional(z.string()),
   batchSize: z.number().int().min(1).max(8).default(1)
 });
+
+export const createTxtToImageSchema = z.object({
+  school: z.string().min(1),
+  prompt: z.string().min(1, { message: '需要输入对应的prompt' }),
+  negativePrompt: z.string().default(''),
+  batchSize: z
+    .number()
+    .int({ message: '需要输入为整数' })
+    .min(1)
+    .max(8, { message: '最大为八张' })
+    .default(1),
+  width: z.number().int().min(256).default(512),
+  height: z.number().int().min(256).default(512),
+  steps: z.number().int().min(1).max(100).default(50),
+  initImage: z.string().default('')
+});
