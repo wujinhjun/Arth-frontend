bec57b836f2fc37fe61fa4e5f8c53dd249fe69f3
wujinhjun <wujinhjun@outlook.com>
2023-12-12 00:41:26 +0800

    chore: update the proxy

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,11 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': 'http://localhost:3000'
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     }
   }
 });
