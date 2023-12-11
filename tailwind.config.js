a37ddd12ac221407f1b14f6dd414081e24f94428
wujinhjun <wujinhjun@outlook.com>
2023-12-12 00:40:53 +0800

    chore: update the tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      aspectRatio: {
+        '4/3': '4 / 3',
+        '3/4': '3 / 4',
+        '1/1': '1 / 1',
+        '16/9': '16 / 9',
+        '9/16': '9 / 16'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
