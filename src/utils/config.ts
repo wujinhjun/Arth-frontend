3c15f8a38fa54417940f6e52ee37bb9b2d1248ab
wujinhjun <wujinhjun@outlook.com>
2023-12-15 15:26:56 +0800

    feat: the basic config

---
+++ src/utils/config.ts
@@ -0,0 +1,26 @@
+export const genreConfig = [
+  { name: '文艺复兴' },
+  { name: '巴洛克' },
+  { name: '洛可可' },
+  { name: '新古典主义' },
+  { name: '浪漫主义' },
+  { name: '印象派' }
+];
+
+export const imgRadioListConfig = [
+  {
+    numerator: 1,
+    denominator: 1,
+    name: '1:1'
+  },
+  {
+    numerator: 4,
+    denominator: 3,
+    name: '4:3'
+  },
+  {
+    numerator: 16,
+    denominator: 9,
+    name: '16:9'
+  }
+];
